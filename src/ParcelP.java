da4ff42f6af44464247e8db14b209d30f05081cd
Vegetius <moosifer42@gmail.com>
2016-01-11 18:36:16 +0100

    Added vertices

--- src/ParcelP.java
+++ src/ParcelP.java
@@ -27,13 +27,38 @@ public class ParcelP extends Parcel
         this.setLocation(location);
     }
 
-    @Override
-    protected void construct()
+    private void construct()
     {
         this.add(new Point3D(0,0,0));
         this.add(new Point3D(1,0,0));
         this.add(new Point3D(0,0,1));
         this.add(new Point3D(1,0,1));
         this.add(new Point3D(0,0,2));
+    }
+    
+    public ArrayList<Point3D> getVertices()
+    {
+        ArrayList<Point3D> part1 = new ArrayList<Point3D>();
+    	ArrayList<Point3D> part2 = new ArrayList<Point3D>();
+    	ArrayList<Point3D> result = new ArrayList<Point3D>();
+    	
+    	part1.add(blockLocations.get(0));
+    	part1.add(blockLocations.get(1));
+    	part1.add(blockLocations.get(2));
+    	part1.add(blockLocations.get(3));
+    	
+    	part2.add(blockLocations.get(4));
+    	
+    	setVertices(part1);
+    	setVertices(part2);
+    	
+         
+         for(Point3D point : vertices)
+        {
+            result.add(point.add(location));
+        }
+        
+        
+        return result;
     }
 }
