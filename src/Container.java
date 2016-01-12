70cf95eca7d885b5152669c885e3ad04d775a1b4
philippe554 <philippe.mmd@gmail.com>
2016-01-12 14:08:16 +0100

    update

--- src/Container.java
+++ src/Container.java
@@ -103,22 +103,6 @@ public class Container {
         return newContainer;
     }
 
-    public int emptyPercent(){
-        int emptyCounter = 0;
-        int totalCounter = 0;
-        for (int i = 0; i < container.length; i++) {
-            for (int j = 0; j < container[i].length; j++) {
-                for (int k = 0; k < container[i][j].length; k++) {
-                    totalCounter++;
-                    if (container[i][j][k] == -1) {
-                        emptyCounter++;
-                    }
-                }
-            }
-        }
-        return ((emptyCounter * 100)/totalCounter);
-    }
-
     public ArrayList<Parcel> getContainedParcels() {return containedParcels;}
 
     public static void main(String[] args)
